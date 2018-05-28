4c302147d8e4dd2d2ca526fb3f9e8d952bbdb067
ronifajrian <ronifajrian@gmail.com>
2018-05-28 23:24:56 +0700

    add file number_in_words

--- number_in_words.js
+++ number_in_words.js
@@ -1,8 +1,63 @@
 function numberToWords(number) {
   // Your code here
+  var angka = ["","satu","dua","tiga","empat","lima","enam","tujuh","delapan","sembilan","sepuluh","sebelas"]
+
+  var result = ''
+
+  if (number < 12) {
+    
+    result += angka[number]
+  } else
+  if (number < 20) {
+    result += numberToWords(number-10) + ' belas'
+  } else
+  if (number < 100) {
+    result += numberToWords(Math.floor(number/10)) + ' puluh ' + numberToWords(number % 10)
+  } else
+  if (number < 200) {
+    result += 'seratus' + numberToWords(number - 100)
+  }else
+  if (number < 1000) {
+    result += numberToWords(Math.floor(number / 100)) + ' ratus ' + numberToWords(number % 100) 
+  }else
+  if (number < 2000) {
+    result += 'seribu ' + numberToWords(number - 1000)
+  }else
+  if (number < 1000000) {
+    result += numberToWords(Math.floor(number / 1000)) + ' ribu ' + numberToWords(number % 1000)
+  }else
+  if (number < 1000000000) {
+    result += numberToWords(Math.floor(number / 1000000)) + ' juta ' + numberToWords(number % 1000000)
+  }else
+  if (number < 1000000000000) {
+    result += numberToWords(Math.floor(number / 1000000000)) + ' milyar ' + numberToWords(number % 1000000000)
+  }
+  return result
 }
 
 // Driver code
-console.log(numberToWords(705));
+console.log(numberToWords(11705));
 console.log(numberToWords(1000000));
 console.log(numberToWords(2011845));
+
+console.log(numberToWords(999999999999));
+
+
+
+
+
+/*
+
+  
+  else if(satuan<1000000000) 
+21
+   hasil=hasil+angka(satuan/1000000)+" Juta "+angka(satuan%1000000);
+22
+  else if(satuan>=1000000000) 
+23
+   hasil="Angka terlalu besar, harus kurang dari 1 milyar!";
+24
+  return hasil; 
+
+
+*/

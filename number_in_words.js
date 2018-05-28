210585d74838fef1654481c13d0fee99440e1edf
fajartc02 <fajartc02@gmail.com>
2018-05-28 23:54:53 +0700

    repaired number-in-words(on progress 50%)

--- number_in_words.js
+++ number_in_words.js
@@ -24,6 +24,7 @@ function numberToWords(number) {
     return containNum + numberToWords(valueFinalNumber)
   } else {
     for (var key in satuanUnik) {
+      // console.log(key)
       if (number % satuanUnik[key] === 0) {
         // if (number % satuanUnik[key] === 0) {
           valueFinalNumber = number / satuanUnik[key]
@@ -31,17 +32,31 @@ function numberToWords(number) {
           valueFinalNumber = 0
           return containNum + numberToWords(valueFinalNumber)
         // }
-      }// else if (number >= 20 && number <= 99) {
-      //   valueFinalNumber = number % satuanUnik[key]
-      //   containNum += satuan[valueFinalNumber] + ' ' + key + ' ' + valueFinalNumber
-      //   valueFinalNumber = 0
-      //   return containNum + numberToWords(valueFinalNumber)
-      // }
+      } else if (number >= satuanUnik[key] && number <= 99) {
+        valueFinalNumber = String(number / satuanUnik[key])
+        console.log(valueFinalNumber)
+        var first = valueFinalNumber.slice(0, 1)
+        // console.log(first)
+        var second = valueFinalNumber.slice(2)
+        // console.log(second)
+        var digitAkhir = number / 10
+        var floor = Math.floor(digitAkhir)
+        containNum += satuan[first] + ' ' + key + ' ' + satuan[second]
+        valueFinalNumber = 0
+        return containNum + numberToWords(valueFinalNumber)
+      } else if (number >= satuanUnik[key] && number <= 999 && number > 100) {
+        valueFinalNumber = Math.floor(number / satuanUnik[key])
+        var mod = number % satuanUnik[key]
+        console.log(mod)
+        console.log(valueFinalNumber)
+        containNum += satuan[valueFinalNumber] + ' ' + key + ' '
+        return containNum + numberToWords(mod)
+      }
     }
   }
 }
 
 // Driver code
-console.log(numberToWords(60));
-console.log(numberToWords(1000000));
-console.log(numberToWords(2011845));
+console.log(numberToWords(611));
+console.log(numberToWords(1000001));
+// console.log(numberToWords(2011845));

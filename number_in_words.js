24ace6c1f799e2d99bdb8a6c4a54727ab8936b30
Ari Supriatna <arisupriatna@MacBook-Air-Ari.local>
2018-05-29 00:35:41 +0700

    fix numbers_in_words.js

--- number_in_words.js
+++ number_in_words.js
@@ -1,63 +1,63 @@
 function numberToWords(number) {
-  let words = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh']
+  let words = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas']
   
+  let translate = ''
+
   if (number <= 11) {
-    return words[number]
-  }  
-  
-  // else if (number === 11) {
-  //   return `sebelas`
-  // }
-  
-  else if (number < 20) {
-    var belasan = words[Math.floor(number / 10)]
-    return `${numberToWords(number % 10)} belas`
-  } 
-  
+    translate += words[number]
+  }
+
+  else if (number > 11 && number < 20) {
+    translate += `${words[String(number)[1]]} belas`
+  }
+
   else if (number < 100) {
-    var puluhan = words[Math.floor(number / 10)]
-    return `${puluhan} puluh ${numberToWords(number % 10)}`
-  } 
+    translate += `${numberToWords(Math.floor(number / 10))} puluh ${numberToWords(number % 10)}`
+  }
   
   else if (number < 200) {
-    return `seratus ${numberToWords(number - 100)}`
+    translate += `seratus ${numberToWords(number - 100)}`
   }
-  
+
   else if (number < 1000) {
-    var dibawahSeribu = words[Math.floor(number / 100)]
-    return `${dibawahSeribu} ratus ${numberToWords(number % 100)}`
-  } 
-  
+    translate += `${numberToWords(Math.floor(number / 100))} ratus ${numberToWords(number % 100)}`
+  }
+
   else if (number < 2000) {
-    return `seribu ${numberToWords(number - 1000)}`
+    translate += `seribu ${numberToWords(number - 1000)}`
   }
-  
-  else if (number < 10000) {
-    var dibawahSepuluhRibu = words[Math.floor(number / 1000)]
-    return `${dibawahSepuluhRibu} ribu ${numberToWords(number % 1000)}`
-  } 
-  
-  else if (number < 100000) {
-    var dibawahSeratusRibu = words[Math.floor(number / 10000)]
-    return `${dibawahSeratusRibu} puluh ${numberToWords(number % 10000)}`
-  } 
-  
+
   else if (number < 1000000) {
-    var dibawahSatuJuta = words[Math.floor(number / 1000)]
-    return `${dibawahSatuJuta} ratus ${numberToWords(number % 1000)}`
+    translate += `${numberToWords(Math.floor(number / 1000))} ribu ${numberToWords(number % 1000)}`
   }
-  
+
   else if (number < 1000000000) {
-    var dibawahSepuluhJuta = words[Math.floor(number / 1000000)]
-    return `${dibawahSepuluhJuta} juta ${numberToWords(number % 1000000)}`
+    translate += `${numberToWords(Math.floor(number / 1000000))} juta ${numberToWords(number % 1000000)}`
   }
+
+  else if (number < 1000000000000) {
+    translate += `${numberToWords(Math.floor(number / 1000000000))} miliar ${numberToWords(number % 1000000000)}`
+  }
+
+  else if (number < 1000000000000000) {
+    translate += `${numberToWords(Math.floor(number / 1000000000000))} triliun ${numberToWords(number % 1000000000000)}`
+  }
+
+  return translate
 
 }
 
 // Driver code
-
+console.log(numberToWords(1111))
 console.log(numberToWords(705));
 console.log(numberToWords(1000000));
 console.log(numberToWords(2011845));
-
+console.log(numberToWords(999999999999999)) 
+/*
+sembilan ratus sembilan puluh sembilan tr
+iliun sembilan ratus sembilan puluh sembi
+lan miliar sembilan ratus sembilan puluh
+sembilan juta sembilan ratus sembilan puluh sembilan ribu sembilan ratus sembilan
+puluh sembilan
+*/
 

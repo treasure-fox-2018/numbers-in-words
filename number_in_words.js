6b8d944b07a5f6675993a0f151eff563052ba945
“Ollas2204” <fitrisakinah17@gmail.com>
2018-05-28 16:47:04 +0700

    numbers_in_words

--- number_in_words.js
+++ number_in_words.js
@@ -1,5 +1,24 @@
 function numberToWords(number) {
   // Your code here
+  let nominal=['','Satu','Dua','Tiga','Empat','Lima','Enam','Tujuh','Delapan','Sembilan','Sepuluh','Sebelas','Seratus ','Seribu '];
+    let satuan=[' Belas ',' Puluh ',' Ratus ',' Ribu ',' Juta ',' Milyar ',' Triliun ']
+    if(number<12){
+        return nominal[number]
+    }else if(number<20){
+        return nominal[number%10]+satuan[0]
+    }else if(number<100){
+        return nominal[Math.floor(number/10)]+satuan[1]+nominal[number%10]
+    }else if(number<200){
+        return nominal[12]+numberToWords(number-100)
+    }else if(number<1000){
+        return nominal[Math.floor(number/100)]+satuan[2]+numberToWords(Math.floor(number%100))
+    }else if(number<2000){
+        return nominal[13]+numberToWords(number-1000)
+    }else if(number<1000000){
+        return numberToWords(Math.floor(number/1000))+satuan[3]+numberToWords(Math.floor(number%1000))
+    }else if(number<1000000000){
+        return numberToWords(Math.floor(number/1000000))+satuan[4]+ numberToWords(number%1000000)
+    }
 }
 
 // Driver code
